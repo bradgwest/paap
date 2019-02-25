d0d533c8b02c9ad9d5708490ffe63098002854ae
Brad West <brad.g.west@gmail.com>
2019-02-24 17:12:29 -0700

    Yield all data in one JSON object

--- art/scrape_art/spiders/christies.py
+++ art/scrape_art/spiders/christies.py
@@ -7,7 +7,7 @@ import re
 
 import scrapy
 
-from art.scrape_art.scrape_art.spiders import christies_settings
+from art.scrape_art.spiders import christies_settings
 
 
 class ChristiesCrawler(scrapy.Spider):
@@ -31,18 +31,21 @@ class ChristiesCrawler(scrapy.Spider):
             location = sale.xpath('./div/div/span[contains(@class, "location")]/text()').get()
             total = sale.xpath('./div/div/div[contains(@class, "sale-total")]/text()').get()
 
-            yield {
+            sale_details = {
                 "sale_url": response.url,
                 "sale_status": status,
-                "sale_nunber": number,
+                "sale_number": number,
                 "sale_location": location,
-                "sale_total": total
+                "sale_total": total,
+                "sale_details": None
             }
 
             sale_page = sale.xpath('./div/div/a[text() = "View results"]/@href').get()
             if sale_page is not None:
                 next_page = response.urljoin(sale_page)
-                yield scrapy.Request(next_page, callback=self.parse_redirect_sale_page)
+                yield scrapy.Request(next_page,
+                                     callback=self.parse_redirect_sale_page,
+                                     meta={"sale_details": sale_details})
 
     def parse_redirect_sale_page(self, response):
         """
@@ -51,7 +54,9 @@ class ChristiesCrawler(scrapy.Spider):
         :return:
         """
         all_results_url = response.url + "&ShowAll=true"
-        yield scrapy.Request(all_results_url, callback=self.parse_sale_page)
+        sale_details = response.meta["sale_details"]
+        yield scrapy.Request(all_results_url, callback=self.parse_sale_page,
+                             meta={"sale_details": sale_details})
 
     def parse_sale_page(self, response):
         """
@@ -61,12 +66,14 @@ class ChristiesCrawler(scrapy.Spider):
         """
         js = response.xpath('//script[@type="text/javascript"][contains(text(), "var saleName")]/text()').get()
         try:
-            sale_details = self.parse_js(js)
+            details = self.parse_js(js)
         except json.JSONDecodeError:
-            sale_details = None
+            details = None
 
-        yield {"sale_details": sale_details}
-        # TODO Add get image details
+        sale_details = response.meta["sale_details"]
+        sale_details["sale_details"] = details
+
+        yield sale_details
 
     @staticmethod
     def parse_js(js):
@@ -80,18 +87,11 @@ class ChristiesCrawler(scrapy.Spider):
             starting_bracket = re.search("{", js[list_view_loc:]).regs[0][0]
             start = int(list_view_loc) + int(starting_bracket)
             # first double newline is end of object
-            ending_bracket = re.search("}\\);\n\n", js[start:]).regs[0][0]
+            ending_bracket = re.search("}\\);", js[start:]).regs[0][0]
         except IndexError:
+            return None
+        except AttributeError:
             return None
 
         js_obj = js[start:(start + ending_bracket + 1)]
         return json.loads(js_obj)
-
-    @staticmethod
-    def get_image_urls(sale_details):
-        """
-        Get's image url from sale details
-        :param sale_details:
-        :return:
-        """
-        pass
