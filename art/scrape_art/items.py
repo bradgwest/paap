6593a14fa25af33c97a0af101b4f0ceaa770e8b8
Brad West <brad.g.west@gmail.com>
2019-03-16 18:31:51 -0600

    More expressive item

--- art/scrape_art/items.py
+++ art/scrape_art/items.py
@@ -7,9 +7,15 @@ class ChristiesItem(scrapy.Item):
     """
     An item for Christies data
     """
+    input_url = scrapy.Field()
+    year = scrapy.Field()
+    month = scrapy.Field()
+    category = scrapy.Field()
+    location = scrapy.Field()
+    location_int = scrapy.Field()
     sale_url = scrapy.Field()
     sale_status = scrapy.Field()
     sale_number = scrapy.Field()
-    sale_location = scrapy.Field()
-    sale_total = scrapy.Field()
-    sale_details = scrapy.Field()
+    sale_total_raw = scrapy.Field()
+    sale_details_js = scrapy.Field()
+    sale_details_html = scrapy.Field()
