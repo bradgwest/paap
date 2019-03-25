a3ec1f5ac486444fc2713bb158324da73c75ec51
Brad West <brad.g.west@gmail.com>
2019-03-24 20:32:04 -0600

    Add some plots

--- analysis.R
+++ analysis.R
@@ -33,5 +33,51 @@ art <- raw %>%
 #   select(lot_image_url) %>% 
 #   write_csv(IMAGE_URLS_FILE_OUTPUT, col_names = FALSE)
 
-# TOTAL VALUE - 58 Trillion US dolla dolla bills
+# TOTAL VALUE - 58 Billion US dolla dolla bills
 sum(art$lot_realized_price_usd_now, na.rm = TRUE)
+
+art %>% 
+  filter(lot_realized_price_usd_now < 1000000) %>% 
+  ggplot(mapping = aes(lot_realized_price_usd_now)) +
+  geom_histogram(binwidth = 5000) +
+  theme_bw() +
+  labs(title = "Lot Price (truncated < $1,000,000)") +
+  ylab("Count") +
+  xlab("Sale Price (2019-01 USD) (binwidth = $5,000)") +
+  ggsave("./img/histogram_sale_price.png", device = "png", width = 16, 
+         height = 9, units = "cm", dpi = 300, scale = 1.5)
+
+art %>% 
+  ggplot(mapping = aes(log(lot_realized_price_usd_now))) +
+  geom_histogram(binwidth = 0.1) +
+  theme_bw() +
+  labs(title = "Log Lot Price") +
+  ylab("Count") +
+  xlab("Log Sale Price (2019-01 USD)") +
+  ggsave("./img/histogram_log_sale_price_x2.png", device = "png", width = 16, 
+         height = 9, units = "cm", dpi = 300, scale = .8)
+
+
+art %>% 
+  filter(!is.na(lot_realized_price_usd_now)) %>% 
+  ggplot(mapping = aes(x=sale_category, log(lot_realized_price_usd_now))) +
+  geom_boxplot() +
+  theme_bw() +
+  labs(title = "Log Lot Price by Category") +
+  ylab("Log Sale Price (2019-01 USD)") +
+  xlab("Category") +
+  coord_flip() +
+  ggsave("./img/boxplot_sale_price_by_categopry.png", device = "png", width = 16, 
+         height = 9, units = "cm", dpi = 300, scale = 0.8)
+
+art %>% 
+  filter(!is.na(lot_realized_price_usd_now)) %>% 
+  ggplot(mapping = aes(x=sale_location, log(lot_realized_price_usd_now))) +
+  geom_boxplot() +
+  theme_bw() +
+  labs(title = "Log Lot Price by Auction Location") +
+  ylab("Log Sale Price (2019-01 USD)") +
+  xlab("Location") +
+  coord_flip() +
+  ggsave("./img/boxplot_sale_price_by_location.png", device = "png", width = 16, 
+         height = 9, units = "cm", dpi = 300, scale = 1.5)
