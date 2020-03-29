33b9d312faa920737d1289eaf3576e15b2f4c066
Brad West <brad.g.west@gmail.com>
2020-03-28 18:06:23 -0600

    Rearrange

--- scripts/exchange_rates.py
+++ scripts/exchange_rates.py
@@ -8,8 +8,8 @@ import datetime
 import logging
 import sys
 
-from forex_python import converter
 import cpi
+from forex_python import converter
 
 YEARS = range(2006, 2020)
 MONTHS = range(1, 13)
@@ -20,7 +20,7 @@ C = converter.CurrencyRates()
 CURR_MAP = [[y, m, c, None, None] for y in YEARS for m in MONTHS for c in CURRENCIES]
 
 
-def convert_to_dollars(base_currency='USD'):
+def convert_to_dollars(base_currency="USD"):
     """
     mutates CURR_MAP
     :param base_currency:
@@ -46,18 +46,15 @@ def calculate_inflation(target_date=datetime.date(2019, 1, 1)):
     for l in CURR_MAP:
         if l[3] is not None:
             try:
-                l[4] = cpi.inflate(l[3],
-                                   datetime.date(l[0], l[1], 1),
-                                   to=target_date)
+                l[4] = cpi.inflate(l[3], datetime.date(l[0], l[1], 1), to=target_date)
             except cpi.errors.CPIObjectDoesNotExist:
                 l[4] = l[3]
 
 
 def parse_args(sys_args):
-    parser = argparse.ArgumentParser(
-        description="Convert historical currencies to a target")
+    parser = argparse.ArgumentParser(description="Convert historical currencies to a target")
     parser.add_argument("-c", "--target-currency", default="USD")
-    parser.add_argument("-d", "--target-date", default='2019-01-01')
+    parser.add_argument("-d", "--target-date", default="2019-01-01")
     parser.add_argument("-o", "--output", default="./currency_converter.csv")
     return parser.parse_args(sys_args)
 
@@ -67,7 +64,7 @@ def main():
     args = parse_args(sys.argv[1:])
     convert_to_dollars(args.target_currency)
     calculate_inflation()
-    with open(args.output, 'w') as f:
+    with open(args.output, "w") as f:
         csv_writer = csv.writer(f)
         for l in CURR_MAP:
             csv_writer.writerow(l)
