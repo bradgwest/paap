5aaadf2c52755263bab1b8aa254254400889e1fa
Brad West <brad.g.west@gmail.com>
2020-04-06 08:27:19 -0600

    Add filter for size

--- scripts/download_images.py
+++ scripts/download_images.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 import os
 import time
+import urllib
 from functools import wraps
 from pathlib import Path
 from typing import Callable
@@ -11,6 +12,8 @@ import requests
 DESCRIPTION = """Given an input csv with image urls, download images and save them to a location"""
 CHUNK_SIZE = 1024
 RETRYABLE_STATUS_CODES = frozenset([429])
+IMAGE_QUERY = "?mode=max&h={height}"
+MAX_HEIGHT_DEFAULT = 1024
 
 
 def valid_directory(directory: str) -> str:
@@ -20,9 +23,13 @@ def valid_directory(directory: str) -> str:
 
 
 def build_image_path(directory: str, image_id: str, image_url: str) -> Path:
-    ext = image_url.split(".")[-1]
+    ext = image_url.split(".")[-1].split("?")[0]
     basename = ".".join([image_id, ext])
     return Path(directory) / basename
+
+
+def build_target_url(image_url: str, max_image_height: int) -> str:
+    return urllib.parse.urljoin(image_url, IMAGE_QUERY.format(height=max_image_height))
 
 
 def retry(retry_on_exception: Callable, delay: int = 1, attempts: int = 4, multiplier: int = 2) -> Callable:
@@ -63,13 +70,14 @@ def download_image(image_url: str, image_path: str) -> None:
             f.write(chunk)
 
 
-def main(input_path: str, output_dir: str) -> None:
+def main(input_path: str, output_dir: str, max_height: int) -> None:
     failed = []
 
     with open(input_path) as f:
         reader = csv.reader(f)
         for i, line in enumerate(reader):
-            image_id, image_url = line
+            image_id, base_url = line
+            image_url = build_target_url(base_url, max_height)
             image_path = build_image_path(output_dir, image_id, image_url)
             print("{} - {} -> {}".format(i + 1, image_url, image_path), end="\r")
 
@@ -91,7 +99,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("input", help="Input csv. Two columns (<lot_id>,<image_url>), no header.")
     parser.add_argument("output_dir", help="Directory to save images to.", type=valid_directory)
+    parser.add_argument("--max-height", default=MAX_HEIGHT_DEFAULT, type=int, help="Maximum height (px) to download")
     args = parser.parse_args()
 
     print("Writing images to {}".format(Path(args.output_dir).resolve()))
-    main(args.input, args.output_dir)
+    main(args.input, args.output_dir, args.max_height)
