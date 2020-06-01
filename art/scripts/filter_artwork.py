0c893686d53ccf991123c809347f574a670e5f4a
Brad West <brad.g.west@gmail.com>
2020-05-31 20:24:50 -0600

    Filter out non 2d artwork

--- art/scripts/filter_artwork.py
+++ art/scripts/filter_artwork.py
@@ -1,28 +1,22 @@
 import argparse
+import re
 from pathlib import Path
+
+from typing import List
 
 import pandas as pd
 
 DESCRIPTION = "Filter out artwork that is unsuitable for analysis according to a set of rules"
-RULES = set()
+
+NON_2D_KEYWORDS = re.compile(r"ceramic|bronze|calligraphy|sculpture|wooden|model|furniture|chair|table|glass|jade|carving|rug|carpet|bust|jewelry|bracelet")
 
 
-def rule(func):
-    """Annotation for adding a rule to the set of all rules"""
-    RULES.add(func)
-    return func
+def _is_3d(s: str) -> bool:
+    return bool(re.search(NON_2D_KEYWORDS, s))
 
 
-@rule
-def is_2d(rows: pd.DataFrame) -> pd.DataFrame:
-    """Return rows that are one of the following mediums:"""
-    pass
-
-
-@rule
-def is_known_genre(rows: pd.DataFrame) -> pd.DataFrame:
-    """Return rows with a known genre"""
-    pass
+def is_3d(row: List[str]) -> bool:
+    return any(_is_3d(v) for v in row)
 
 
 def valid_path(path_str: str) -> Path:
@@ -32,17 +26,29 @@ def valid_path(path_str: str) -> Path:
     return path
 
 
-def main(input_csv: Path, output_csv: Path, rules: list) -> None:
-    pass
+def main(input_json: Path, is_2d_json: Path, output_json: Path) -> None:
+    df = pd.read_json(input_json, orient="records")
+    is_2d = pd.read_json(is_2d_json, orient="records")
+
+    # filter out sales that weren't 2d
+    is_2d = is_2d[is_2d.sale_is_2d]
+    df = pd.merge(df, is_2d, on=("sale_number", "sale_url"), how="inner")
+
+    # filter out sales that have some of the disallowed keywords
+    df = df[~df[["lot_description", "lot_artist", "lot_medium"]].apply(is_3d, axis=1)]
+    df = df.drop("sale_is_2d", axis=1)
+
+    print("Writing {} rows".format(df.shape[0]))
+    df.to_json(output_json, orient="records")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument("input_csv", type=valid_path,
-                        help="Path to input csv with raw scrapped data, header on first row")
-    parser.add_argument("output_csv", type=valid_path, help="Path to save output csv containing only filtered data")
-    parser.add_argument("--rules", nargs="*", default="*", help="Filtering rules to apply",
-                        choices=[r.__name__ for r in RULES])
+    parser.add_argument("input_json", type=valid_path,
+                        help="Path to input json, the output from clean_data.py")
+    parser.add_argument("is_2d_json", type=valid_path,
+                        help="Path to is_2d json, the output from is_2d.py")
+    parser.add_argument("output_json", type=str, help="Path to save output json containing only filtered data")
     args = parser.parse_args()
 
-    main(args.input_csv, args.output_csv, args.rules)
+    main(args.input_json, args.is_2d_json, args.output_json)
