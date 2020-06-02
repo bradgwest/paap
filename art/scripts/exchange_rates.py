537584ee324623ddb0daf7b165bfe41b8db4c69d
Brad West <brad.g.west@gmail.com>
2020-06-02 08:39:54 -0600

    Update exchange_rates.py

--- art/scripts/exchange_rates.py
+++ art/scripts/exchange_rates.py
@@ -1,75 +1,89 @@
 import argparse
 import csv
 import datetime
-import logging
+from typing import Sequence, List
+from collections import namedtuple
 
 import cpi
 from forex_python import converter
 
 YEARS = range(2006, 2020)
 MONTHS = range(1, 13)
-CURRENCIES = ("EUR", "GBP", "HKD", "AUD", "CHF", "INR", "CNY", "USD")
+DESCRIPTION = """Get the dollar equivalent of currencies at a given date, and the inflation relative to today. Output
+is a csv with 5 columns: year, month, currency, dollar_equivalent, inflation.
+
+currency is the currency which we wish to convert to dollars.
+dollar_equivalent is the price of that currency, in USD at that year and month.
+inflation is the multiple by which to multiply a dollar in that year/month to get it's equivalent worth at the target
+date.
+""".strip()
+
+
+class Currency(object):
+    EUR = "EUR"
+    GBP = "GBP"
+    HKD = "HKD"
+    AUD = "AUD"
+    CHF = "CHF"
+    INR = "INR"
+    CNY = "CNY"
+    USD = "USD"
+    ALL = (EUR, GBP, HKD, AUD, CHF, INR, CNY, USD)
+
+
+BASE_CURRENCY = Currency.USD
 
 C = converter.CurrencyRates()
 
-CURR_MAP = [[y, m, c, None, None] for y in YEARS for m in MONTHS for c in CURRENCIES]
+ExchangeRateSnapshot = namedtuple("ExchangeRateSnapshot", ["year", "month", "currency", "dollar_equivalent", "inflation"])
 
 
-def convert_to_dollars(base_currency="USD"):
-    """
-    mutates CURR_MAP
-    :param base_currency:
-    :return:
-    """
-    for l in CURR_MAP:
-        # First of the month
-        if l[1] == 1 and l[2] == CURRENCIES[0]:
-            logging.info("Getting currencies for {}".format(l[0]))
-        date_obj = datetime.datetime(l[0], l[1], 1)
-        if l[2] == "USD":
-            l[3] = 1
-            continue
-        elif date_obj > datetime.datetime.now():
-            continue
-        try:
-            l[3] = C.convert(l[2], base_currency, 1, date_obj)
-        except converter.RatesNotAvailableError:
-            continue
+def convert_to_dollars(year: int, month: int, currency: str) -> float:
+    if currency == Currency.USD:
+        return 1
+
+    dt = datetime.datetime(year, month, 1)
+    if dt > datetime.datetime.now():
+        raise ValueError("{} is in the future".format(dt.strptime("%Y-%m-%d")))
+
+    # might raise converter.RatesNotAvailableError
+    return C.convert(currency, BASE_CURRENCY, 1, dt)
 
 
-def calculate_inflation(target_date=datetime.date(2019, 1, 1)):
-    for l in CURR_MAP:
-        if l[3] is not None:
-            try:
-                l[4] = cpi.inflate(l[3], datetime.date(l[0], l[1], 1), to=target_date)
-            except cpi.errors.CPIObjectDoesNotExist:
-                l[4] = l[3]
+def calculate_inflation(year: int, month: int, target_date: datetime.datetime) -> float:
+    # might raise cpi.errors.CPIObjectDoesNotExist
+    return cpi.inflate(1, datetime.date(year, month, 1), to=target_date)
 
 
-def parse_args(sys_args):
-    parser = argparse.ArgumentParser(description="Convert historical currencies to a target currency")
-    parser.add_argument("-c", "--target-currency", default="USD")
-    parser.add_argument("-d", "--target-date", default="2019-01-01")
-    parser.add_argument("-o", "--output", default="./currency_converter.csv")
-    return parser.parse_args(sys_args)
+def make_currency_map(years: Sequence[int], months: Sequence[int], currencies: Sequence[str]) -> List[ExchangeRateSnapshot]:
+    return [ExchangeRateSnapshot(y, m, c, None, None) for y in years for m in months for c in currencies]
 
 
-def main(target_currency, target_date, output):
-    convert_to_dollars(target_currency)  # TODO. Ew, this is not pure either
-    calculate_inflation(target_date)  # TODO Ew. This is not pure at all
+def main(output, target_date):
+    rows = []
+    for y in YEARS:
+        for m in MONTHS:
+            # calculate how much more dollars are worth today
+            inflation = calculate_inflation(y, m, target_date)
+            for c in Currency.ALL:
+                # calculate how many dollars, 1 currency unit is worth
+                dollar_equivalent = convert_to_dollars(y, m, c)
+                rows.append(
+                    ExchangeRateSnapshot(y, m, c, dollar_equivalent, inflation)
+                )
 
     with open(output, "w") as f:
-        csv_writer = csv.writer(f)
-        for l in CURR_MAP:
-            csv_writer.writerow(l)
+        writer = csv.DictWriter(f)
+        writer.writeheader(ExchangeRateSnapshot._fields)
+        writer.writerows(rows)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert historical currencies to a target")
-    parser.add_argument("-c", "--target-currency", default="USD")
-    parser.add_argument("-d", "--target-date", default="2019-01-01",
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-d", "--target-date", default="2020-01-01",
                         type=lambda x: datetime.datetime.strptime(x, "%Y-%m-%d"))
-    parser.add_argument("-o", "--output", default="./currency_converter.csv")
+
     args = parser.parse_args()
 
-    main(args.target_currency, args.target_date, args.output)
+    main(args.output, args.target_date)
