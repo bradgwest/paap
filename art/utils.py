c184f5ca2feae94a9fcacebeb14e4976a62669ab
Brad West <brad.g.west@gmail.com>
2020-06-06 11:22:00 -0600

    add utilities

---
+++ art/utils.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+def valid_path(path_str: str) -> Path:
+    path = Path(path_str)
+    if not (path.exists() and path.is_file()):
+        raise ValueError("{} is not a valid path".format(path))
+    return path
+
+
+# All data is stored as JSON, oriented as records
+def read_data(path: Path) -> pd.DataFrame:
+    pd.read_json(path, orient="records")
+
+
+def write_data(df: pd.DataFrame, path: str) -> None:
+    df.to_json(path)
