99ee7ad75be320d526c714f1a65778e2f63673ad
Brad West <brad.g.west@gmail.com>
2020-07-13 22:58:49 -0600

    Package up some data

--- nn/dcec_paint/run_model.sh
+++ nn/dcec_paint/run_model.sh
@@ -4,6 +4,10 @@ set -ex
 
 # echo "test" > /tmp/test.txt
 # gsutil cp /tmp/test.txt gs://paap/nn/dcec/
+mkdir -p data
+gsutil cp gs://paap/nn/dcec_paint/data/photos_and_prints.tar.gz /build/data
+tar -xzf /build/data/photos_and_prints.tar.gz
+rm /build/data/photos_and_prints.tar.gz
 
 # Run the model
 /build/dcec/bin/python /build/DCEC.py photos_and_prints --assert-gpu
